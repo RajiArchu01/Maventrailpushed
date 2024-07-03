e6c620193f5e2dc04685e1a8be82aa159dcbf355
RajiArchu01 <rajibnec01@gmail.com>
2024-07-03 14:19:39 +0530

    raji pushed to github

---
+++ src/test/java/com/maventry/MavenBrowserLaunch.java
@@ -0,0 +1,18 @@
+package com.maventry;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MavenBrowserLaunch {
+	public static void main(String[] args) {
+	
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+	}
+}
